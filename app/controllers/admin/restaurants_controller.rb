ebbec5e062f7ccd497d1db13b03d6f24d2641dd7
anna770822 <anna770822@hotmail.com>
2018-01-10 15:18:18 +0800

    add bootstrap template to all form

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -6,7 +6,7 @@ class Admin::RestaurantsController < ApplicationController
   before_action :set_restaurant, :only => [:show, :edit, :update, :destroy]
 
   def index
-    @restaurants = Restaurant.page(params[:page]).per(9)
+    @restaurants = Restaurant.page(params[:page]).per(10)
   end
   
   def new
