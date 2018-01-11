24c18760b71aa3a88e06d035c004419b3309d5c5
anna770822 <anna770822@hotmail.com>
2018-01-11 12:44:08 +0800

    create categories and default admin user in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Category
+
+Category.destroy_all
+
+category_list = [
+  {name: "Chinese"},
+  {name: "Japanese"},
+  {name: "Italian"},
+  {name: "Mexican"},
+  {name: "Vegetarian"},
+  {name: "American"},
+  {name: "Fusion"}
+]
+
+category_list.each do |category|
+  Category.create(name: category[:name])
+end
+
+puts "Category created"
+
+#Default admin
+
+User.create(email: "anna770822@gmail.com", password:"123456", role: "admin")
+
+puts "Default admin created"
