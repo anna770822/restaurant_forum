0e2b1a479dc60041b43ac611a3eb2d86c2a79a0d
anna770822 <anna770822@hotmail.com>
2018-01-11 13:08:32 +0800

    add has_many and belongs_to to model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,5 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name, :tel, :address, :opening_hours, :description
   mount_uploader :image, PhotoUploader
   
+  belong_to :category
 end
