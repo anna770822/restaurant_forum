24dc67230c5551bf8fcbd59e6f3ead42b6f81702
anna770822 <anna770822@hotmail.com>
2018-01-12 16:12:12 +0800

    remove-rails-field_with_errors-wrapper

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#remove rails field_with_errors wrapper
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
