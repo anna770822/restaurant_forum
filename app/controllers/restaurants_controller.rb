099b719d56d4d92ddd085529daac91d09b319cba
anna770822 <anna770822@hotmail.com>
2018-01-15 12:49:08 +0800

    show pages done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,8 @@ class RestaurantsController < ApplicationController
   before_action :authenticate_user!
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
-  
   end
-  
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
