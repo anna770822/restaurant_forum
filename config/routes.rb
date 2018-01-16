06cd5fa254c998b8c2dd5799fb9a9db135985771
anna770822 <anna770822@hotmail.com>
2018-01-16 14:09:28 +0800

    add profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only:[:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index"
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
   end
   resources :categories, only: [:show]
-
+  
 
   
   namespace :admin do 
