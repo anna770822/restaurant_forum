372ea2e074b1e0744c87591d146a7f8ab3c0ca38
anna770822 <anna770822@hotmail.com>
2018-01-17 21:06:20 +0800

    push to origin master

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,4 @@ class Category < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name, :case_sensitive => false
   has_many :restaurants, dependent: :restrict_with_error
-
 end
