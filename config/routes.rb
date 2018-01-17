e3668d4181c2ae01c19ba59a859f66df1b92c4fc
anna770822 <anna770822@hotmail.com>
2018-01-17 14:42:01 +0800

    setup routes for feeds and dashboard pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :users, only:[:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index"
+
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
-  end
-  resources :categories, only: [:show]
-  
 
+    collection do
+      get :feeds
+    end
+
+    member do 
+      get :dashboard
+    end
+    
+  end
+
+  resources :categories, only: [:show]
   
   namespace :admin do 
     resources :restaurants
